6323f74497e5b4256830dd177200916915255029
charuzain <charuabhizain@gmail.com>
2022-09-21 00:33:03 -0400

    order history added

--- Store.java
+++ Store.java
@@ -1,5 +1,8 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class Store {
 
@@ -14,6 +17,13 @@ public class Store {
     itemsInStore.add(item1);
     itemsInStore.add(item2);
     itemsInStore.add(item3);
+
+ ArrayList<History> purchaseHistoryList = new ArrayList<>();
+    // History history = new History();
+     String message;
+      do {
+        
+      
 
     System.out.println("List of all the items in store is as: ");
     System.out.println("====================================");
@@ -31,20 +41,38 @@ public class Store {
     System.out.println("How many " + itemsInStore.get(productId - 1).productName + " would you like to buy ?");
 
     int orderQuantity = sc.nextInt();
+    // ArrayList<History> purchaseHistoryList = new ArrayList<>();
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    LocalDateTime now = LocalDateTime.now();
 
     if (orderQuantity <= itemsInStore.get(productId - 1).quantity) {
+
       int quantityLeft = itemsInStore.get(productId - 1).quantity - orderQuantity;
-      itemsInStore.get(productId - 1).quantity = quantityLeft;
+    // update quantity  
+    itemsInStore.get(productId - 1).setQuantity(quantityLeft);
+
+      // itemsInStore.get(productId - 1).quantity = quantityLeft;
       double totalCost = itemsInStore.get(productId - 1).price * orderQuantity;
-      System.out.println("Your total cost is " + totalCost + " $");
+      System.out.println("Total cost to buy " + orderQuantity + " "+ itemsInStore.get(productId - 1).productName +   " "+  totalCost + " $");
+      History itemPurchaseHistory = new History(itemsInStore.get(productId - 1).productName, orderQuantity, totalCost,
+          dtf.format(now));
+      purchaseHistoryList.add(itemPurchaseHistory);
+       
+    System.out.println();
     }
     else {
       System.out.println("Not sufficient inventory");
 
     }
-    
+     System.out.println("Do you want to continue to buy more items press Y for Yes or N for No");
+            message = sc.next();
 
-
+} 
+while (message.equals("y") || message.equals("Y"));
+ 
+     for (int i = 0; i < purchaseHistoryList.size(); i++) {
+                System.out.println("Purchased "+purchaseHistoryList.get(i).productName+ " : " + purchaseHistoryList.get(i).quantity+ " : " + purchaseHistoryList.get(i).puchaseTime);
+            }
 
     
 
